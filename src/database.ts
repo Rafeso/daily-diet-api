b3785486a329b394cfa0f522f7aa4a10b6462721
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-05-16 12:57:15 -0300

    feat(env): treating env with Zod

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
 import { Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import { env } from './env'
 
 const app = fastify()
 
@@ -8,7 +9,7 @@ app.get('/hello', () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
