af199c3fdaaae84c6b7118e0108fcfb5c781c7cf
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-05-16 12:21:57 -0300

    feat(migrations): add `created_at` column at users table

---
+++ db/migrations/20230516151825_add-created-at-to-users.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (t) => {
+    t.timestamp('created_at')
+      .defaultTo(knex.fn.now())
+      .notNullable()
+      .after('email')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('users', (t) => {
+    t.dropColumn('created_at')
+  })
+}
