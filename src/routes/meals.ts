aad50523ea804b7e18168131cf9e55c02a67499c
Rafael Feitosa <rafaefeitosa@gmail.com>
2023-05-17 12:06:49 -0300

    feat(cookies): add authentication with cookies

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -3,43 +3,55 @@ import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
 import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
-    const createMealBodySchema = z.object({
-      name: z.string(),
-      description: z.string(),
-      is_on_diet: z.enum(['yes', 'no']),
-    })
+  app.post(
+    '/',
+    { preHandler: checkSessionIdExists },
+    async (request, reply) => {
+      const sessionId = request.cookies.sessionId
 
-    const { name, description, is_on_diet } = createMealBodySchema.parse(
-      request.body,
-    )
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        is_on_diet: z.enum(['yes', 'no']),
+      })
 
-    await knex('meals').insert({
-      id: randomUUID(),
-      name,
-      description,
-      is_on_diet,
-    })
+      const { name, description, is_on_diet } = createMealBodySchema.parse(
+        request.body,
+      )
 
-    return reply.status(201).send({ message: 'Successfully created meal!' })
-  })
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        description,
+        is_on_diet,
+        user_id: sessionId,
+      })
 
-  app.get('/', async () => {
-    const meals = await knex('meals').select()
+      return reply.status(201).send({ message: 'Successfully created meal!' })
+    },
+  )
+
+  app.get('/', { preHandler: checkSessionIdExists }, async (request, reply) => {
+    const sessionId = request.cookies.sessionId
+
+    const meals = await knex('meals').where('user_id', sessionId).select()
 
     return meals
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: checkSessionIdExists }, async (request) => {
+    const sessionId = request.cookies.sessionId
+
     const getMealsParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getMealsParamsSchema.parse(request.params)
 
-    const meal = await knex('meals').where('id', id).first()
+    const meal = await knex('meals').where({ id, user_id: sessionId }).first()
 
     return { meal }
   })
